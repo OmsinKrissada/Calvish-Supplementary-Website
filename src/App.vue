ca901d38b9e25c9f85f04de42a404159c0ae8050
Krissada Singhakachain <46844213+OmsinKrissada@users.noreply.github.com>
2022-10-03 12:16:06 +0700

    planning

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,27 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted, ref } from 'vue';
+import Banner from './components/icons/Banner.vue';
+import LiquidEmeraldStack from './components/icons/LiquidEmeraldStack.vue';
+
+const xp = ref(0);
+
+onMounted(() => {
+
+});
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div class="flex flex-col min-h-screen p-10 bg-teal-800 text-white">
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <Banner />
+    <header class="flex flex-col items-center space-y-5">
+      <h1 class="font-fira text-5xl">Calvish [bean]</h1>
+      <div class="flex space-x-5">
+        <LiquidEmeraldStack />
+        <h3 class="font-medium text-2xl text-slate-300">Top XP Event <span class="underline text-emerald-200">Season
+            1</span></h3>
+      </div>
+    </header>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
---
+++ src/components/icons/LiquidEmeraldStack.vue
@@ -0,0 +1,8 @@
+<template>
+	<div class="relative w-10 h-10 -translate-y-1">
+		<img src="../../assets/liquid_emerald.webp" class="absolute w-20 rotate-12 subpixel-antialiased"
+			style="image-rendering: pixelated;" alt="">
+		<img src="../../assets/liquid_emerald.webp" class="absolute w-16 mt-2 ml-3 rotate-45 subpixel-antialiased"
+			style="image-rendering: pixelated;" alt="">
+	</div>
+</template>
