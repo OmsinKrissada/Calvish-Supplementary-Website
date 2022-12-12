7a638ae295a8e1acc172e86fc3e872f1e390a019
Krissada Singhakachain <46844213+OmsinKrissada@users.noreply.github.com>
2022-12-12 19:18:38 +0700

    removed unused meta tags

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,12 +2,12 @@
 
 	<Head>
 		<Title>Calvish Guild</Title>
-		<Meta name="description" content="Description here" />
-		<Meta name="twitter:title" content="HW Tracker | Beta" />
+		<!-- <Meta name="description" content="Description here" /> -->
+		<Meta name="twitter:title" content="Calvish Guild" />
 		<Meta name="og:type" content="website" />
-		<Meta name="og:title" content="HW Tracker | Beta" />
-		<Meta name="og:description" content="สมุดจดการบ้านของห้อง 6/1" />
-		<Meta name="twitter:description" content="สมุดจดการบ้านของห้อง 6/1" />
+		<Meta name="og:title" content="Calvish Guild" />
+		<!-- <Meta name="og:description" content="สมุดจดการบ้านของห้อง 6/1" /> -->
+		<!-- <Meta name="twitter:description" content="สมุดจดการบ้านของห้อง 6/1" /> -->
 	</Head>
 	<!-- <div class="min-h-screen bg-[#1A202C]"> -->
 	<!-- <div> -->
