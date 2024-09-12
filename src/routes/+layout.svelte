f24ba10f1bda9308b4ad1e279d4976b6746317d3
Krissada Singhakachain <omsin_47@hotmail.com>
2024-09-12 13:12:50 +0700

    added vercel speed insights collection

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,8 @@
 	import { dev } from '$app/environment';
 	import { inject } from '@vercel/analytics';
 	inject({ mode: dev ? 'development' : 'production' });
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+	injectSpeedInsights();
 
 	import { fly } from 'svelte/transition';
 	import { env } from '$env/dynamic/public';
